bba04427fb3cd33ee21139785b18bb64818360ec
Ajay Singh <ajaysingh52011025@gmail.com>
2022-05-03 12:55:28 +0530

    Add files via upload

---
+++ jsnew2.js
@@ -0,0 +1,17 @@
+function openme() {
+	// Find the element
+	x=document.getElementById('demo');
+	//Option1.Change the style attribute directly.
+	x.style.display="block";
+	//Option2.Chage the class
+	//x.className="open";
+}
+function closeme() {
+	// Find the element
+	x=document.getElementById('demo');
+	//Option1.Change the style attribute directly.
+	x.style.display="none";
+	//Option2.Chage the class
+	//x.className="closed";
+
+}
